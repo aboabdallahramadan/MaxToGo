ab5cebc971e03ef1cbc579cd24108662e30dbf75
mohammad ramadan <aboabdallahramadan@gmail.com>
2024-10-15 12:17:06 +0300

    adding language button

--- components/layout/LayoutHeader.jsx
+++ components/layout/LayoutHeader.jsx
@@ -1,10 +1,10 @@
 "use client";
 import Image from "next/image";
-import { Link } from "@/i18n/routing";
+import Link from "next/link";
 import NavLink from "@/components/NavLink";
 import { useTranslations } from "next-intl";
 import PrimaryLink from "../PrimaryLink";
-import {FaBars} from "react-icons/fa";
+import {FaBars, FaGlobe} from "react-icons/fa";
 import { useState } from "react";
 
 
@@ -14,9 +14,10 @@ const LayoutHeader = () => {
     let session = true;
 
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const [isLanguageMenuOpen, setIsLanguageMenuOpen] = useState(false);
   return (
-    <div className="container fixed">
-      <header className="h-12 flex justify-between pt-2">
+    <header className="fixed w-full">
+        <div className="container h-12 flex justify-between pt-2">
           <div className="h-100 w-40">
           <Image src="/images/logo.png" width={150} height={50} alt="logo" style={{ width: "auto", height: "100%"}} priority/>
           </div>
@@ -28,8 +29,17 @@ const LayoutHeader = () => {
               <NavLink link="#contact">{t("Contact")}</NavLink>
           </ul>
           </nav>
+          
           <div className="flex justify-between items-center">
             {(session ? <PrimaryLink link={"/login"}>{t("Login")}</PrimaryLink> : <PrimaryLink link={"/app"}>{t("GoApp")}</PrimaryLink>)}
+            <button
+              type='button'
+              id='language-dropdown-button'
+              className='ml-4'
+              aria-controls='language-menu'
+              aria-expanded='false'
+              onClick={() => setIsLanguageMenuOpen((prev) => !prev)}
+            ><FaGlobe className="text-secondary"/></button>
             <button
               type='button'
               id='mobile-dropdown-button'
@@ -39,6 +49,26 @@ const LayoutHeader = () => {
               onClick={() => setIsMobileMenuOpen((prev) => !prev)}
             ><FaBars className="text-secondary"/></button>
           </div>
+          {
+            isLanguageMenuOpen && (
+              <div id='Language-menu' className="absolute right-0 top-10 bg-secondary">
+                <div className='space-y-1 px-2 pb-3 pt-2'>
+                  <ul className="flex flex-col justify-between items-center gap-4">
+                    <li>
+                      <Link href="/en" className="font-bold ml-4 px-4 py-2 border border-transparent hover:border hover:border-primary hover:text-primary whitespace-nowrap rounded">
+                        {t("English")} 
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/sv" className="font-bold ml-4 px-4 py-2 border border-transparent hover:border hover:border-primary hover:text-primary whitespace-nowrap rounded">
+                        {t("Swedish")} 
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            )
+          }
           {
             isMobileMenuOpen && (
               <div id='mobile-menu' className="absolute right-0 top-10 bg-secondary">
@@ -53,8 +83,8 @@ const LayoutHeader = () => {
               </div>
             )
           }
-      </header>
-    </div>
+      </div>
+    </header>
   )
 }
 
