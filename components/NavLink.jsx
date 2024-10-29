d05046d2a39b67877aac0a49b7860dab2c41011c
mohammad ramadan <aboabdallahramadan@gmail.com>
2024-10-29 18:18:54 +0300

    getting navLinks bigger

--- components/NavLink.jsx
+++ components/NavLink.jsx
@@ -2,7 +2,7 @@ import { Link } from "@/i18n/routing";
 const NavLink = ({children,link,func}) => {
   return (
     <li>
-      <Link href={link} className="font-bold ml-4 px-4 py-2 border border-transparent hover:border hover:border-primary hover:text-primary whitespace-nowrap rounded" 
+      <Link href={link} className="font-bold ml-4 px-4 py-2 border border-transparent hover:border hover:border-primary hover:text-primary whitespace-nowrap rounded lg:text-xl" 
       onClick={func}
     >
         {children}    
