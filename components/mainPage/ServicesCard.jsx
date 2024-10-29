4f27f3e201cba30d431bd38abaf19ca022861d9e
mohammad ramadan <aboabdallahramadan@gmail.com>
2024-10-29 18:19:45 +0300

    deleting background of services

--- components/mainPage/ServicesCard.jsx
+++ components/mainPage/ServicesCard.jsx
@@ -1,7 +1,7 @@
 const ServicesCard = ({Icon,title,text}) => {
   return (
     <div className="bg-secondary">
-      <div className="logoMark p-7 z-[10] w-[18rem] flex relative flex-col items-center justify-between gap-8 hover:shadow-2xl hover:shadow-primary border border-gray-400 rounded sm:w-[22rem] lg:w-[24rem]">
+      <div className="p-7 z-[10] w-[18rem] flex relative flex-col items-center justify-between gap-8 hover:shadow-2xl hover:shadow-primary border border-gray-400 rounded sm:w-[22rem] lg:w-[24rem]">
         <div className="flex items-center justify-center gap-2">
             <Icon className="text-primary text-3xl"/>
             <h3 className="text-1xl text-primary">{title}</h3>
