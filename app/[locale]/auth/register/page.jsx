55d535145ead918b338f4b28c8a4c28c92d97b5a
mohammad ramadan <aboabdallahramadan@gmail.com>
2024-11-11 17:48:46 +0300

    added ramavtal

--- app/[locale]/auth/register/page.jsx
+++ app/[locale]/auth/register/page.jsx
@@ -1,10 +1,11 @@
 "use client";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import AuthPageHeader from "@/components/AuthPageHeader";
 import GoBackButton from "@/components/GoBackButton";
 import { useTranslations } from "next-intl";
 import { FaBuilding, FaEnvelope, FaKey, FaMarker, FaPhone, FaEye, FaEyeSlash, FaUpload } from "react-icons/fa";
 import { BsArrowLeft, BsArrowRight, BsPersonFill } from "react-icons/bs";
+import MustRead from "@/components/auth/MustRead";
 
 const page = () => {
   const validateFileType = (file, allowedTypes) => {
@@ -26,16 +27,14 @@ const page = () => {
     organizationNumber: '',
     buyOrSell: '',
     description: '',
-    policy: false
+    policy: false,
+    ramavtal: false
   });
   const [errors, setErrors] = useState({});
 
   const t = useTranslations("Auth");
-
-  const [hasReadTerms, setHasReadTerms] = useState(false);
-  const termsRef = useRef(null);
-
-  
+  const tTerms = useTranslations("Auth.Terms");
+  const tRamavtal = useTranslations("Auth.ramavtal");
 
   const handlePreviousPage = () => {
     setPageNumber(pageNumber - 1);
@@ -138,6 +137,10 @@ const page = () => {
     else if (pageNumber === 3) {
       if (!formData.policy) {
         newErrors.policy = t("PleaseAcceptTerms");
+        newErrors.ramavtal = t("PleaseAcceptTerms");
+      }
+      if (!formData.ramavtal) {
+        newErrors.ramavtal = t("PleaseAcceptTerms");
       }
     }
   
@@ -145,14 +148,7 @@ const page = () => {
     return Object.keys(newErrors).length === 0;
   };
 
-  const handleTermsScroll = (e) => {
-    const element = e.target;
-    const isAtBottom = Math.abs(element.scrollHeight - element.scrollTop - element.clientHeight) < 1;
-    
-    if (isAtBottom) {
-      setHasReadTerms(true);
-    }
-  };
+
 
   // State to manage password visibility
   const [showPassword, setShowPassword] = useState(false);
@@ -380,45 +376,10 @@ const page = () => {
           </div>
           <div className={pageNumber == 3 ? "block" : "hidden"}>
             <div className="mt-6">
-              <h3 className="text-xl font-bold text-[#3B82F6] mb-4">{t('Terms.title')}</h3>
-              <div 
-                ref={termsRef}
-                onScroll={handleTermsScroll}
-                className="bg-[#1A1A1A] p-6 rounded-lg max-h-96 overflow-y-auto mb-4"
-              >
-                {Object.keys(t.raw('Terms.sections')).map((section) => (
-                  <div key={section} className="mb-6">
-                    <h4 className="text-lg font-bold text-white mb-2">
-                      {t(`Terms.sections.${section}.title`)}
-                    </h4>
-                    {Object.entries(t.raw(`Terms.sections.${section}.content`)).map(([key, _]) => (
-                      <p key={key} className="text-gray-400 mb-2">
-                        {t(`Terms.sections.${section}.content.${key}`)}
-                      </p>
-                    ))}
-                  </div>
-                ))}
-              </div>
+              <MustRead name={"policy"} t0={tTerms} t={t} formData={formData} handleInputChange={handleInputChange} errors={errors}/>
+              <MustRead name={"ramavtal"} t0={tRamavtal} t={t} formData={formData} handleInputChange={handleInputChange} errors={errors}/>
             </div>
-            <div className="relative w-full mt-6 flex justify-start items-center gap-2">
-            <input 
-              name="policy" 
-              id="policy" 
-              className="bg-transparent accent-primary" 
-              type="checkbox"                 
-              checked={formData.policy}
-              onChange={handleInputChange} 
-              disabled={!hasReadTerms}
-              required
-            />
-                <label htmlFor="policy" className="text-primary text-center">{t("Agree")} {t("Policy")} </label>
-            </div >
-            {errors.policy && <p className="text-red-500 text-sm">{errors.policy}</p>}
-            {!hasReadTerms && (
-              <p className="text-sm text-primary mt-2">
-                {t("PleaseReadTerms")}
-              </p>
-            )}
+            
           </div>
           <div className="flex justify-end items-center gap-4 mt-4">
             <div className={`bg-primary py-2 px-4 rounded-lg cursor-pointer ${pageNumber > 0 ? "block" : "hidden"}`} onClick={handlePreviousPage}><BsArrowLeft className="text-secondary text-lg font-bold"/></div>
---
+++ components/auth/MustRead.jsx
@@ -0,0 +1,64 @@
+"use client";
+import { useRef,useState } from "react";
+
+
+const MustRead = ({t, t0, name, formData, handleInputChange, errors}) => {
+    const [hasReadTerms, setHasReadTerms] = useState(false);
+    const termsRef = useRef(null);
+
+    const handleTermsScroll = (e) => {
+        const element = e.target;
+        const isAtBottom = Math.abs(element.scrollHeight - element.scrollTop - element.clientHeight) < 1;
+        
+        if (isAtBottom) {
+        setHasReadTerms(true);
+        }
+    };
+
+  return (
+    <div>
+        <div>
+        <h3 className="text-xl font-bold text-[#3B82F6] mb-4">{t0('title')}</h3>
+        <div 
+            ref={termsRef}
+            onScroll={handleTermsScroll}
+            className="bg-[#1A1A1A] p-6 rounded-lg max-h-96 overflow-y-auto mb-4"
+        >
+            {Object.keys(t0.raw('sections')).map((section) => (
+            <div key={section} className="mb-6">
+                <h4 className="text-lg font-bold text-white mb-2">
+                {t0(`sections.${section}.title`)}
+                </h4>
+                {Object.entries(t0.raw(`sections.${section}.content`)).map(([key, _]) => (
+                <p key={key} className="text-gray-400 mb-2">
+                    {t0(`sections.${section}.content.${key}`)}
+                </p>
+                ))}
+            </div>
+            ))}
+        </div>
+        </div>
+        <div className="relative w-full mt-6 flex justify-start items-center gap-2">
+        <input 
+            name={name} 
+            id={name} 
+            className="bg-transparent accent-primary" 
+            type="checkbox"                 
+            checked={formData[name]}
+            onChange={handleInputChange} 
+            disabled={!hasReadTerms}
+            required
+        />
+            <label htmlFor={name} className="text-primary text-center">{t("Agree")} {t0("object")} </label>
+        </div >
+        {errors[name] && <p className="text-red-500 text-sm">{errors[name]}</p>}
+        {!hasReadTerms && (
+            <p className="text-sm text-primary mt-2">
+            {t("PleaseReadTerms")}
+            </p>
+        )}
+    </div>
+  )
+}
+
+export default MustRead
